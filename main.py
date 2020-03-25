7b2e1dad5c44270ff51235796e1f9335318e2237
Zokhan <sotnikov.zaxar@bk.ru>
2020-03-25 15:59:43 +0700

    Move and level
    Add level and add control

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import pygame
 from control import Control
 from snake import Snake
+from gui import Gui
+
 pygame.init()
 
 # Create display game
-window = pygame.display.set_mode((600, 600))
+window = pygame.display.set_mode((441, 441))
 pygame.display.set_caption("Snake")
 control = Control()
 snake = Snake()
+gui = Gui()
 speed = 0
+
+
 # Cycle so that the window does not close
 while control.flag_game:
     control.control()
     # Create a way out of the game
     window.fill(pygame.Color("black"))
     snake.draw_snake(window)
-    if speed % 100 == 0:
+    gui.draw_level(window)
+    if speed % 100 == 0 and control.flag_pause:
         snake.move(control)
+        snake.check_end_window()
+        snake.animation()
     speed += 2
-    pygame.display.flip()   
+    pygame.display.flip()
--- snake.py
+++ snake.py
@@ -1,19 +1,36 @@
 import pygame
-
+from gui import Gui
+from control import Control
 
 class Snake:
     def __init__(self):
         self.head = [45, 45]
-
+        self.body = [[45, 45], [34, 45], [23, 45]]
     def move(self, control):
         if control.flag_direction == "RIGHT":
-            self.head[0] += 10
+            self.head[0] += 11
         elif control.flag_direction == "LEFT":
-            self.head[0] -= 10
+            self.head[0] -= 11
         elif control.flag_direction == "UP":
-            self.head[1] -= 10
+            self.head[1] -= 11
         elif control.flag_direction == "DOWN":
-            self.head[1] += 10
+            self.head[1] += 11
+    def animation(self):
+        self.body.insert(0, list(self.head))
+        self.body.pop()
 
     def draw_snake(self, window):
-        pygame.draw.rect(window, pygame.Color("GREEN"), pygame.Rect(self.head[0], self.head[1], 10, 10))
+        for segment in self.body:
+            pygame.draw.rect(window, pygame.Color("GREEN"), pygame.Rect(segment[0], segment[1], 10, 10))
+
+    def check_end_window(self):
+        if self.head[0] == 419:
+            self.head[0] = 23
+        elif self.head[0] == 12:
+            self.head[0] = 419
+        elif self.head[1] == 23:
+            self.head[1] = 419
+        elif self.head[1] == 419:
+            self.head[1] = 34
+
+
